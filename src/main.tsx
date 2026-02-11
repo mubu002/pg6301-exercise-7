b5eae52699fc5c7547ac17688e280f4e3f1dd4d9
mubu002 <mustafa_buga1@hotmail.com>
2026-02-11 17:09:25 +0100

    Klienten skal nå hente tasks fra /api/tasks.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,39 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Task = {
+  description: string;
+  completed: boolean;
+};
 
 export function Application() {
-  return <h1>Task application</h1>;
+  const [tasks, setTasks] = useState<Task[]>([
+    { description: "Create client", completed: true },
+    { description: "Fetch from server", completed: false },
+  ]);
+
+  async function loadTasks() {
+    const res = await fetch("/api/tasks");
+    setTasks(await res.json());
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>Task application</h1>
+      <ul>
+        {tasks.map(({ description, completed }, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={completed} readOnly />
+            {description}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
