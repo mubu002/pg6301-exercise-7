23956288f704c1ec56cb9bf1b381df12b5459774
mubu002 <mustafa_buga1@hotmail.com>
2026-02-11 17:43:59 +0100

    Satt opp Vite Proxy, slik at backend og frontend er koblet opp.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+});
