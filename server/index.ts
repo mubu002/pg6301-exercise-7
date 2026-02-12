f54e04a3c03689943960c2649a573d178d11e0cb
mubu002 <mustafa_buga1@hotmail.com>
2026-02-12 01:40:18 +0100

    Erstattet array med Mongo, nå henter vi fra mongo i GET, Lagre i Mongo i POST, Oppdatere i Mongo i PUT.

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import type { TaskItem } from "../src/shared/TaskItem.ts";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = new Hono();
 
@@ -13,43 +13,40 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/";
 const client = new MongoClient(MONGODB_URL);
 const connection = await client.connect();
 const taskDb = connection.db("task_application");
-
-/* -------------------- TEMP ARRAY (fortsatt i bruk) -------------------- */
-
-const tasks: TaskItem[] = [
-  { description: "Create client", completed: true },
-  { description: "Fetch from server", completed: true },
-  { description: "Deploy to Heroku", completed: false },
-];
+const taskCollection = taskDb.collection<TaskItem>("tasks");
 
 /* -------------------- API ROUTES -------------------- */
 
-app.get("/api/tasks", (c) => {
+app.get("/api/tasks", async (c) => {
+  const tasks = await taskCollection.find().toArray();
   return c.json(tasks);
 });
 
 app.post("/api/tasks", async (c) => {
-  const task = await c.req.json();
+  const { description, completed } = await c.req.json();
 
-  const newTask: TaskItem = {
-    description: task.description,
-    completed: task.completed ?? false,
+  const task: TaskItem = {
+    description,
+    completed: completed ?? false,
   };
 
-  tasks.push(newTask);
+  await taskCollection.insertOne(task);
 
   return c.newResponse(null, 201);
 });
 
-app.put("/api/tasks/:index", async (c) => {
-  const index = parseInt(c.req.param("index"));
+app.put("/api/tasks/:id", async (c) => {
+  const id = c.req.param("id");
   const { completed } = await c.req.json();
 
-  if (!tasks[index]) {
+  const result = await taskCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { completed } },
+  );
+
+  if (result.matchedCount === 0) {
     return c.newResponse("Not found", 404);
   }
-
-  tasks[index].completed = completed;
 
   return c.newResponse(null, 204);
 });
